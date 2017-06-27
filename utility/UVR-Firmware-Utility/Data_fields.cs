c74f0826e050837080ff43965a4b49668ffa361b
anmrtz <martinez070@gmail.com>
2017-06-26 17:54:33 -0700

    Added firmware utility program
    Reworked directory structure

---
+++ utility/UVR-Firmware-Utility/Data_fields.cs
@@ -0,0 +1,397 @@
+﻿using System;
+
+namespace UVR_Firmware_Utility
+{
+	public partial class MainWindow
+	{
+		public readonly int MEMORY_MAX_BYTES = 16777216;
+
+		#region TextBlockData
+		#region BNO055
+		public double yaw, pitch, roll;
+		public double Yaw
+		{
+			get
+			{ return yaw; }
+
+			set
+			{
+				if (yaw != value)
+				{
+					yaw = value;
+					OnPropertyChanged("yaw");
+				}
+			}
+		}
+		public double Pitch
+		{
+			get
+			{ return pitch; }
+
+			set
+			{
+				if (pitch != value)
+				{
+					pitch = value;
+					OnPropertyChanged("pitch");
+				}
+			}
+		}
+		public double Roll
+		{
+			get
+			{ return roll; }
+
+			set
+			{
+				if (roll != value)
+				{
+					roll = value;
+					OnPropertyChanged("roll");
+				}
+			}
+		}
+
+		public Int16 q_w, q_x, q_y, q_z;
+		public Int16 Q_w
+		{
+			get { return q_w; }
+
+			set
+			{
+				if (q_w != value)
+				{
+					q_w = value;
+					OnPropertyChanged("q_w");
+				}
+			}
+		}
+		public Int16 Q_x
+		{
+			get { return q_x; }
+
+			set
+			{
+				if (q_x != value)
+				{
+					q_x = value;
+					OnPropertyChanged("q_x");
+				}
+			}
+		}
+		public Int16 Q_y
+		{
+			get { return q_y; }
+
+			set
+			{
+				if (q_y != value)
+				{
+					q_y = value;
+					OnPropertyChanged("q_y");
+				}
+			}
+		}
+		public Int16 Q_z
+		{
+			get { return q_z; }
+
+			set
+			{
+				if (q_z != value)
+				{
+					q_z = value;
+					OnPropertyChanged("q_z");
+				}
+			}
+		}
+
+		public double a_x, a_y, a_z, a_net;
+		public double A_x
+		{
+			get { return a_x; }
+
+			set
+			{
+				if (a_x != value)
+				{
+					a_x = value;
+					OnPropertyChanged("a_x");
+				}
+			}
+		}
+		public double A_y
+		{
+			get { return a_y; }
+
+			set
+			{
+				if (a_y != value)
+				{
+					a_y = value;
+					OnPropertyChanged("a_y");
+				}
+			}
+		}
+		public double A_z
+		{
+			get { return a_z; }
+
+			set
+			{
+				if (a_z != value)
+				{
+					a_z = value;
+					OnPropertyChanged("a_z");
+				}
+			}
+		}
+		public double A_net
+		{
+			get { return a_net; }
+
+			set
+			{
+				if (a_net != value)
+				{
+					a_net = value;
+					OnPropertyChanged("a_net");
+				}
+			}
+		}
+		#endregion
+		#region ADXL377
+		public double h_x, h_y, h_z, h_lpz;
+		public double H_x
+		{
+			get { return h_x; }
+
+			set
+			{
+				if (h_x != value)
+				{
+					h_x = value;
+					OnPropertyChanged("h_x");
+				}
+			}
+		}
+		public double H_y
+		{
+			get { return h_y; }
+
+			set
+			{
+				if (h_y != value)
+				{
+					h_y = value;
+					OnPropertyChanged("h_y");
+				}
+			}
+		}
+		public double H_z
+		{
+			get { return h_z; }
+
+			set
+			{
+				if (h_z != value)
+				{
+					h_z = value;
+					OnPropertyChanged("h_z");
+				}
+			}
+		}
+		public double H_LPZ
+		{
+			get { return h_lpz; }
+
+			set
+			{
+				if (h_lpz != value)
+				{
+					h_lpz = value;
+					OnPropertyChanged("h_lpz");
+				}
+			}
+		}
+		#endregion
+		#region BMP280
+		public double temp, pres, rel_alt, lp_pres;
+		public double Temp
+		{
+			get { return temp; }
+			set
+			{
+				if (temp != value)
+				{
+					temp = value;
+					OnPropertyChanged("temp");
+				}
+			}
+		}
+		public double Pres
+		{
+			get { return pres; }
+			set
+			{
+				if (pres != value)
+				{
+					pres = value;
+					OnPropertyChanged("pres");
+				}
+			}
+		}
+		public double LP_Pres
+		{
+			get { return lp_pres; }
+			set
+			{
+				if (lp_pres != value)
+				{
+					lp_pres = value;
+					OnPropertyChanged("lp_pres");
+				}
+			}
+		}
+		public double Rel_Alt
+		{
+			get { return rel_alt; }
+			set
+			{
+				if (rel_alt != value)
+				{
+					rel_alt = value;
+					OnPropertyChanged("rel_alt");
+				}
+			}
+		}
+		#endregion
+		#region W25Q128FV
+		public int bytes_written, bytes_downloaded; 
+		public uint dq_size;
+		public int Bytes_written
+		{
+			get { return bytes_written; }
+			set
+			{
+				if (bytes_written != value)
+				{
+					bytes_written = value;
+					OnPropertyChanged("bytes_written");
+				}
+			}
+		}
+		public int Bytes_downloaded
+		{
+			get { return bytes_downloaded; }
+			set
+			{
+				if (bytes_downloaded != value)
+				{
+					bytes_downloaded = value;
+					OnPropertyChanged("bytes_downloaded");
+				}
+			}
+		}
+		public uint DQ_Size
+		{
+			get { return dq_size; }
+			set
+			{
+				if (dq_size != value)
+				{
+					dq_size = value;
+					OnPropertyChanged("dq_size");
+				}
+			}
+
+		}
+		#endregion
+		#region COM
+		public int err_count, btr;
+		public int Err_count
+		{
+			get { return err_count; }
+			set
+			{
+				if (err_count != value)
+				{
+					err_count = value;
+					OnPropertyChanged("err_count");
+				}
+			}
+		}
+
+		public int Btr
+		{
+			get { return btr; }
+			set
+			{
+				if (btr != value)
+				{
+					btr = value;
+					OnPropertyChanged("btr");
+				}
+			}
+		}
+
+		#endregion
+		#region Time
+		public uint time_elapsed, time_between_measurements, sensor_delay;
+		public uint Time_elapsed
+		{
+			get { return time_elapsed; }
+			set
+			{
+				if (time_elapsed != value)
+				{
+					time_elapsed = value;
+					OnPropertyChanged("time_elapsed");
+				}
+			}
+		}
+		public uint Time_between_measurements
+		{
+			get { return time_between_measurements; }
+			set
+			{
+				if (time_between_measurements != value)
+				{
+					time_between_measurements = value;
+					OnPropertyChanged("time_between_measurements");
+				}
+			}
+		}
+		public uint Sensor_delay
+		{
+			get { return sensor_delay; }
+			set
+			{
+				if (sensor_delay != value)
+				{
+					sensor_delay = value;
+					OnPropertyChanged("sensor_delay");
+				}
+			}
+		}
+		#endregion
+		#region Diodes
+		public bool diode_on;
+		public bool Diode_on
+		{
+			get { return diode_on; }
+			set
+			{
+				if (diode_on != value)
+				{
+					diode_on = value;
+					OnPropertyChanged("diode_on");
+				}
+			}
+		}
+		#endregion
+
+		#endregion
+	}
+
+}
---
+++ utility/UVR-Firmware-Utility/MainWindow.xaml.cs
@@ -0,0 +1,356 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace UVR_Firmware_Utility
+{
+
+	public partial class MainWindow : Window, INotifyPropertyChanged
+	{
+		private System.IO.Ports.SerialPort serialPort1;
+		private List<Byte> recieved_bytes = new List<Byte>();
+		private Dictionary<char, int> tokens = new Dictionary<char, int>();
+
+		private bool writing_to_file = false;
+
+		private System.IO.BinaryWriter mem_file;
+
+		private double hx_curr, hy_curr, hz_curr, hx_zero, hy_zero, hz_one, ref_pres;
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected void OnPropertyChanged(string propertyName)
+		{
+			if (PropertyChanged != null)
+				PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+		public MainWindow()
+		{
+			InitializeComponent();
+			serialPort1 = new System.IO.Ports.SerialPort();
+			
+			serialPort1.BaudRate = 115200;
+			//set threshold to minimum size of data burst
+			serialPort1.ReceivedBytesThreshold = 6;
+			serialPort1.ReadBufferSize = 65536;
+			
+			this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived); 
+			comboBoxPorts.ItemsSource = System.IO.Ports.SerialPort.GetPortNames();
+			
+			tokens.Add('t', 4); // temperature
+			tokens.Add('p', 4); // pressure
+
+			tokens.Add('a', 6); // low-g accelerometer
+			tokens.Add('q', 8); // quaternion orientation
+
+			tokens.Add('c', 4); // millisecond time elapsed measurement
+
+			tokens.Add('s', 4); // sensor read time
+			tokens.Add('m', 4); // bytes written to memory
+			tokens.Add('b', 4); // bytes in payload data queue
+
+			tokens.Add('d', 1); // board is now sending logged data to the pc over serial 
+			tokens.Add('f', 1); // board has finished sending logged data to the pc over serial
+			
+			tokens.Add('h', 6); // high-g accelerometer
+
+			tokens.Add('z', 2); // z-axis rest acceleration
+			tokens.Add('x', 4); // launch pad reference pressure
+
+			tokens.Add('u', 4); // uv on/off state
+		}
+
+		private void buttonConnect_Click(object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				serialPort1.Open();
+			}
+			catch (System.Exception ex) 
+			{
+				MessageBox.Show("ERROR: Could not open serial port:\n" + ex.Message);
+				return;
+			}
+
+			comboBoxPorts.IsEnabled = false;
+			buttonConnect.IsEnabled = false;
+			buttonDisconnect.IsEnabled = true;
+			buttonDownload.IsEnabled = true;
+			buttonRelPres.IsEnabled = true;
+		}
+
+		private void buttonDownload_Click(object sender, RoutedEventArgs e)
+		{
+			if (!writing_to_file)
+			{
+				try
+				{
+					mem_file = new System.IO.BinaryWriter(File.Open("flash_contents.mem",FileMode.Create));
+				}
+				catch (System.Exception ex)
+				{
+					MessageBox.Show("ERROR! Could not open csv file:\n" + ex.Message);
+					return;
+				}
+				Bytes_downloaded = 0;
+				serialPort1.Write("d");
+				buttonDownload.Content = "Stop Download";
+				buttonDisconnect.IsEnabled = false;
+			}
+			else
+			{
+				end_download();
+				serialPort1.Write("a");
+			}
+		}
+
+		private void end_download()
+		{
+			serialPort1.DataReceived -= serialPort1_DataReceived;
+
+			Bytes_downloaded += recieved_bytes.Count;
+
+			for (int i = 0; i < recieved_bytes.Count; i++)
+			{
+				mem_file.Write(recieved_bytes[i]);
+
+				//csv_file.Write(recieved_bytes[i]);
+			}
+			recieved_bytes.Clear();
+			serialPort1.DiscardInBuffer();
+			serialPort1.ReceivedBytesThreshold = 6;
+
+			writing_to_file = false;
+
+			mem_file.Close();
+			mem_file = null;
+
+			buttonDownload.Content = "Start Download";
+			buttonDisconnect.IsEnabled = true;
+			serialPort1.DataReceived += serialPort1_DataReceived;
+		}
+
+		
+		private void buttonCalH_Click(object sender, RoutedEventArgs e)
+		{
+			hx_zero = hx_curr;
+			hy_zero = hy_curr;
+			hz_one = hz_curr;
+		}
+
+		private void buttonRelPres_Click(object sender, RoutedEventArgs e)
+		{
+			ref_pres = Pres;
+		}
+
+		private void comboBoxPorts_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			if (comboBoxPorts.SelectedItem.ToString() != "")
+			{
+				buttonConnect.IsEnabled = true;
+				serialPort1.PortName = comboBoxPorts.SelectedItem.ToString();
+			}
+			else
+				buttonConnect.IsEnabled = false;
+		}
+
+		private void buttonDisconnect_Click(object sender, RoutedEventArgs e)
+		{
+			if (!serialPort1.IsOpen)
+				return;
+			serialPort1.Close();
+			comboBoxPorts.IsEnabled = true;
+			buttonConnect.IsEnabled = true;
+			buttonDisconnect.IsEnabled = false;
+			buttonDownload.IsEnabled = false;
+			buttonRelPres.IsEnabled = false;
+		}
+
+		// parse data values from recieved frame
+		private void process_frame(char token)
+		{
+			byte[] bytes = recieved_bytes.ToArray();
+			switch (token)
+			{
+				case 'c':
+					Time_between_measurements = Time_elapsed;
+					Time_elapsed = System.BitConverter.ToUInt32(bytes, 1);
+					Time_between_measurements = Time_elapsed - Time_between_measurements;
+					break;
+				case 'a':
+					A_x = (double)System.BitConverter.ToInt16(bytes, 1) / 100.0;
+					A_y = (double)System.BitConverter.ToInt16(bytes, 3) / 100.0;
+					A_z = (double)System.BitConverter.ToInt16(bytes, 5) / 100.0;
+					A_net = Math.Sqrt(Math.Pow(A_x, 2.0) + Math.Pow(A_y, 2.0) + Math.Pow(A_z, 2.0));
+					break;
+				case 'q':
+					Q_w = System.BitConverter.ToInt16(bytes, 1);
+					Q_x = System.BitConverter.ToInt16(bytes, 3);
+					Q_y = System.BitConverter.ToInt16(bytes, 5);
+					Q_z = System.BitConverter.ToInt16(bytes, 7);
+
+					// process quaternion into euler angles and update displayed euler values
+
+					// normalize the quaternion
+					double magnitude = Math.Sqrt(Q_w * Q_w + Q_x * Q_x + Q_y * Q_y + Q_z * Q_z);
+					double qx = Q_x / magnitude;
+					double qy = Q_y / magnitude;
+					double qz = Q_z / magnitude;
+					double qw = Q_w / magnitude;
+
+					double ysqr = qy * qy;
+
+					// roll (x-axis rotation)
+					double t0 = +2.0 * (qw * qx + qy * qz);
+					double t1 = +1.0 - 2.0 * (qx * qx + ysqr);
+					Roll = Math.Atan2(t0, t1) * (180.0 / Math.PI);
+
+					// pitch (y-axis rotation)
+					double t2 = +2.0 * (qw * qy - qz * qx);
+					t2 = t2 > 1.0 ? 1.0 : t2;
+					t2 = t2 < -1.0 ? -1.0 : t2;
+					Pitch = Math.Asin(t2) * (180.0 / Math.PI);
+
+					// yaw (z-axis rotation)
+					double t3 = +2.0 * (qw * qz + qx * qy);
+					double t4 = +1.0 - 2.0 * (ysqr + qz * qz);
+					Yaw = Math.Atan2(t3, t4) * (180.0 / Math.PI);
+
+					break;
+				case 't':
+					Temp = (double)System.BitConverter.ToInt32(bytes, 1) / 100.0;
+					break;
+				case 'p':
+					Pres = (double)System.BitConverter.ToUInt32(bytes, 1);
+					if (ref_pres == 0.0)
+						ref_pres = Pres;
+					Rel_Alt = 44330.0 * (1.0 - Math.Pow(Pres / ref_pres, 0.1903));
+					break;
+
+				case 'h':
+					hx_curr = System.BitConverter.ToUInt16(bytes, 1);
+					hy_curr = System.BitConverter.ToUInt16(bytes, 3);
+					hz_curr = System.BitConverter.ToUInt16(bytes, 5);
+
+					
+					const double ratio = 400.0 / 65535.0;
+					const double half = 65535.0 / 2.0;
+
+					H_x = (double)(hx_curr - half) * ratio;
+					H_y = (double)(hy_curr - half) * ratio;
+					H_z = (double)(hz_curr - half) * ratio;
+
+					break;
+				case 'z':
+					h_lpz = System.BitConverter.ToUInt16(bytes, 1);
+					H_LPZ = (double)(h_lpz - half) * ratio;
+					break;
+				case 'x':
+					LP_Pres = (double)System.BitConverter.ToUInt32(bytes, 1);
+					break;
+				case 'm':
+					Bytes_written = System.BitConverter.ToInt32(bytes, 1);
+					break;
+				case 'b':
+					DQ_Size = System.BitConverter.ToUInt32(bytes, 1);
+					break;
+				case 's':
+					Sensor_delay = System.BitConverter.ToUInt32(bytes, 1);
+					break;
+				case 'u':
+					int diode_state = System.BitConverter.ToInt32(bytes, 1);
+					Diode_on = (diode_state != 0);
+					break;
+				case 'd':
+					if (!writing_to_file)
+					{
+						// board will wait 10ms before transmitting memory contents
+						serialPort1.DiscardInBuffer();
+						serialPort1.ReceivedBytesThreshold = 256;
+						writing_to_file = true;
+						
+						// immediately clear recieved bytes buffer and exit the function in the special case
+						// that the pc requests a memory download
+						recieved_bytes.Clear();
+						return;
+					}
+					break;
+				case 'f':
+					if (writing_to_file)
+					{
+						end_download();
+					}
+					break;
+				default:
+					break;
+			}
+			// will raise an exception if invalid token used
+			recieved_bytes.RemoveRange(0, tokens[token] + 2);
+		}
+
+		private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+		{
+			try
+			{
+				Btr = serialPort1.BytesToRead;
+				// retrieve bytes from serial read buffer
+				while (serialPort1.BytesToRead > 0)
+					recieved_bytes.Add((Byte)serialPort1.ReadByte());
+			}
+			// if serial port is closed while trying to read from it, then abort
+			catch (System.InvalidOperationException)
+			{
+				return;
+			}
+
+			if (writing_to_file)
+			{
+				Bytes_downloaded += recieved_bytes.Count;
+				for (int i = 0; i < recieved_bytes.Count; i++)
+				{
+					mem_file.Write(recieved_bytes[i]);
+					//csv_file.Write(recieved_bytes[i]);
+				}
+				recieved_bytes.Clear();
+				return;
+			}
+
+			while (true)
+			{
+				// scan recieved bytes for a valid data token
+				while (recieved_bytes.Count > 0 &&
+					!tokens.ContainsKey((char)recieved_bytes[0]))
+				{
+					// if invalid token at start of recieved bytes, then remove it
+					recieved_bytes.RemoveAt(0);
+				}
+
+				// if not enough bytes to read for full data burst, then exit and 
+				// wait for next transmission
+				if (recieved_bytes.Count == 0)
+					break;
+				char front_char = (char)recieved_bytes[0];
+				if (recieved_bytes.Count < tokens[front_char] + 2)
+				{
+					break;
+				}
+
+				// if there is a data frame error, then remove the invalid bytes
+				// and restart token scanner
+				if ((char)recieved_bytes[tokens[front_char] + 1] != ',')
+				{
+					recieved_bytes.RemoveRange(0, tokens[front_char] + 1);
+					Err_count++;
+					continue;
+				}
+
+				// process the (hopefully) complete data frame
+				process_frame(front_char);
+			}
+		}
+	}
+}
